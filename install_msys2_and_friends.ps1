028a6af2dc5a7dac7a1b0a080b97a4c7ba6871a4
Elliot Saba <staticfloat@gmail.com>
2015-06-20 22:05:46 -0700

    Yay more better msys installation stuffage

--- install_msys2_and_friends.ps1
+++ install_msys2_and_friends.ps1
@@ -1,47 +1,5 @@
 # To get verbose status messages, run `$VerbosePreference = "Continue"` in PowerShell
 
-# Install Cygwin and SSH, set it to startup and add an authorized_key as a buildbot
-# function Install-Cygwin {
-#     param ( $CygDir="c:\cygwin", $arch="x86", $password )
-#     if(!(Test-Path -Path $CygDir -PathType Container)) {
-#         Write-Verbose "Creating directory $CygDir"
-#         New-Item -Type Directory -Path $CygDir -Force
-#     }
-#     Write-Verbose "Downloading http://cygwin.com/setup-$arch.exe"
-#     $client = new-object System.Net.WebClient
-#     $client.DownloadFile("http://cygwin.com/setup-$arch.exe", "$CygDir\setup-$arch.exe" )
-   
-#     Write-Verbose "Installing Cygwin and ssh"
-#     Start-Process -wait -FilePath "$CygDir\setup-$arch.exe" -ArgumentList "-q -g -l $CygDir -s http://mirror.mit.edu/cygwin/ -R c:\cygwin -P openssh"
-
-#     $env:Path = $env:Path + ";$CygDir\bin"
-#     Write-Verbose "Setting up SSH server configuration"
-#     bash --login -c "ssh-host-config -y -w $password"
-
-#     Write-Verbose "Configuring windows firewall for SSH"
-#     $fwtest = netsh advfirewall firewall show rule name="Cygwin SSH"
-#     if( $fwtest.count -lt 5 ) {
-#         netsh advfirewall firewall add rule profile=any name="Cygwin SSH" dir=in localport=22 protocol=TCP action=allow
-#     }
-
-#     Write-Verbose "Starting SSHd"
-#     Start-Service sshd -ErrorAction SilentlyContinue
-
-#     Write-Verbose "Installing SSH key fingerprint"
-#     if( !(Test-Path -Path "$CygDir\home\Administrator\.ssh" -PathType Container) ) {
-#         Write-Verbose "Creating directory $CygDir\home\Administrator\.ssh"
-#         New-Item -Type Directory -Path "$CygDir\home\Administrator\.ssh" -Force
-#     }
-#     $client.DownloadFile("http://sophia.e.ip.saba.us/julia_rsa.pub", "$CygDir\home\Administrator\.ssh\authorized_keys" )
-
-#     Write-Verbose "chown'ing authorized_keys"
-#     chown Administrator ~/.ssh/authorized_keys
-#     chmod 0600 ~/.ssh/authorized_keys
-#     chown Administrator ~/.ssh
-#     chmod 0600 ~/.ssh
-# }
-
-$VerbosePreference = "Continue"
 # Install Msys2 and most of a toolchain
 function Install-Msys2 {
     param ( $arch="i686" )
@@ -95,7 +53,9 @@ function Install-Msys2 {
     # Start doing the ssh-configuration dance
     Write-Verbose "Configuring ssh..."
     &$msyspath\usr\bin\bash -lc @'
-PRIV_USER="ssh_user"
+set -x
+PRIV_USER="cyg_server"
+UNPRIV_USER="sshd"
 
 # Generate a host key for ourselves
 ssh-keygen -A
@@ -104,8 +64,8 @@ ssh-keygen -A
 tmp_pass=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | dd count=14 bs=1 2>/dev/null)
 
 # Add ssh user
-add=$(if ! net user "${PRIV_USER}" >/dev/null; then echo "//add"; fi)
-net user "${PRIV_USER}" "${tmp_pass}" ${add} //fullname:"${PRIV_NAME}" //homedir:"$(cygpath -w $/var/empty)" //yes
+add=$(if ! net user "${PRIV_USER}" >/dev/null 2>&1; then echo "//add"; fi)
+net user "${PRIV_USER}" "${tmp_pass}" ${add} //fullname:"${PRIV_NAME}" //homedir:$(cygpath -w $/var/empty) //yes
 
 # Add user to the Administrators group if necessary
 if ! (net localgroup "Administrators" | grep -q '^'"${PRIV_USER}"'$'); then
@@ -113,14 +73,44 @@ if ! (net localgroup "Administrators" | grep -q '^'"${PRIV_USER}"'$'); then
 fi
 
 # Don't let passwords expire on this joker
-passwd -e "${PRIV_USER}""
+passwd -e "${PRIV_USER}"
 
 # Set required privileges
-/mingw64/bin/editrights -a SeAssignPrimaryTokenPrivilege -u "${PRIV_USER}"
-/mingw64/bin/editrights -a SeCreateTokenPrivilege -u "${PRIV_USER}"
-/mingw64/bin/editrights -a SeTcbPrivilege -u "${PRIV_USER}"
-/mingw64/bin/editrights -a SeDenyRemoteInteractiveLogonRight -u "${PRIV_USER}"
-/mingw64/bin/editrights -a SeServiceLogonRight -u "${PRIV_USER}"
+ER=/mingw*/bin/editrights
+$ER -a SeAssignPrimaryTokenPrivilege -u "${PRIV_USER}"
+$ER -a SeCreateTokenPrivilege -u "${PRIV_USER}"
+$ER -a SeTcbPrivilege -u "${PRIV_USER}"
+$ER -a SeDenyRemoteInteractiveLogonRight -u "${PRIV_USER}"
+$ER -a SeServiceLogonRight -u "${PRIV_USER}"
+
+add=$(if ! net user "${UNPRIV_USER}" >/dev/null; then echo "//add"; fi)
+net user "${UNPRIV_USER}" ${add} //fullname:"${UNPRIV_NAME}" //homedir:$(cygpath -w /var/empty) //active:no
+
+cygrunsrv -R sshd >/dev/null 2>&1 || true
+cygrunsrv -I sshd -d 'MSYS2 sshd' -p /usr/bin/sshd -a -D -y tcpip -u "${PRIV_USER}" -w "${tmp_pass}"
+net start sshd
+'@
+
+    Write-Verbose "Configuring windows firewall for SSH"
+    $fwtest = netsh advfirewall firewall show rule name="SSH"
+    if( $fwtest.count -lt 5 ) {
+        netsh advfirewall firewall add rule profile=any name="SSH" dir=in localport=22 protocol=TCP action=allow
+    }
+
+    Write-Verbose "Installing SSH key fingerprint"
+    if( !(Test-Path -Path "$msyspath\home\Administrator\.ssh" -PathType Container) ) {
+        Write-Verbose "Creating directory $msyspath\home\Administrator\.ssh"
+        New-Item -Type Directory -Path "$msyspath\home\Administrator\.ssh" -Force
+    }
+    (new-object net.webclient).DownloadFile("http://sophia.e.ip.saba.us/julia_rsa.pub", "$msyspath\home\Administrator\.ssh\authorized_keys" )
+
+    Write-Verbose "chown'ing authorized_keys"
+
+    &$msyspath\usr\bin\bash -lc @'
+chown Administrator ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+chown Administrator ~/.ssh
+chmod 0600 ~/.ssh
 '@
 }
 
